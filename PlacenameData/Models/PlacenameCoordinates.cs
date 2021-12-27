d5394a3549d8cd4659c87d2057faf915403e1a73
tookie bunten <davidtookiebunten@gmail.com>
2021-12-27 18:28:23 +0000

    adding data classes

---
+++ PlacenameData/Models/PlacenameCoordinates.cs
@@ -0,0 +1,20 @@
+﻿namespace PlacenameData.Models
+{
+    public class PlacenameCoordinates
+    {
+
+        //id NameId os_sheet	os_grid_10	osgb_east	osgb_north	easting(x)	northing(y)	longitude	latitude
+
+        public int Id { get; set; }
+        public string NameId { get; set; } // from PlacenameData.Models.Placename.NameId
+        public string OsSheet { get; set; } 
+        public string OsGrid10 { get; set; } // Ordance survey 10 figure grid ref
+        public string OsGbEast { get; set; }
+        public string OsGbNorth { get; set; }
+        public string Easting { get; set; }
+        public string Northing { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+
+    }
+}
---
+++ PlacenameData/Models/PlacenameNotes.cs
@@ -0,0 +1,12 @@
+﻿namespace PlacenameData.Models
+{
+    public class PlacenameNotes
+    {
+        //id	name_id	variation_id	notes
+
+        public int Id { get; set; }
+        public string NameId { get; set; }// from PlacenameData.Models.Placename.NameId
+        public string VariationId { get; set; }// from PlacenameData.Models.PlacenameVariations.Id
+        public string Notes { get; set; }
+    }
+}
