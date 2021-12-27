9ef111bf7f1b2b07121cc0e4505f8eadc9ff0205
tookie bunten <davidtookiebunten@gmail.com>
2021-12-27 17:56:13 +0000

    Adding new data model

---
+++ PlacenameData/Models/PlacenameVariations.cs
@@ -0,0 +1,16 @@
+﻿namespace PlacenameData.Models
+{
+    public class PlacenameVariations
+    {
+
+        //id NameId VariationSpelling VariationSource VariationSourceDate VariationComments
+
+        public int Id { get; set; }
+        public string NameId { get; set; } //from PlacenameData.Models.Placename.NameId
+        public string VariationSpelling { get; set; } //spelling variation of place-name
+        public string VariationSource { get; set; } //source of variation, web address, journal, book etc
+        public DateTime VariationSourceDate { get; set; } //just needs to be set to 4 digit year
+        public string VariationComments { get; set; } //user comments about the variation
+
+    }
+}
